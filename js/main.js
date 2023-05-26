e5ad7979a7022466f04d0232863a4584e0b93a00
yuwei yang <yang0347@algonquinlive.com>
2023-05-26 12:45:58 -0400

    s

--- js/main.js
+++ js/main.js
@@ -6,7 +6,9 @@ console.log(document.title)
 //finding an element by id (box)
 const infoPara = document.getElementById("info")
 console.log(infoPara)
+console.dir(infoPara) // if console.log() shows us the string verson of the object(if refresh the web is not work) we can use console.dir() method 
 
+infoPara.textContent = "Abhay"
 
 
 
