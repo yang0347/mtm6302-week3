ba21223b9048da645a1c150edfbf0ed651ffef8e
yuwei yang <yang0347@algonquinlive.com>
2023-05-26 12:38:15 -0400

    s

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log("Js file is here")
+//console.log("Js file is here") (check the website is working, it will delet)
+
+//accessing document property (光标在最前面， option + 上下左右键 可以串行串列)
+console.log(document.title)
+
+//finding an element by id (box)
+const infoPara = document.getElementById("info")
+console.log(infoPara)
+
+
+
+
